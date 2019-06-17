4dc3b4ab3e77ece37d3bd1e4a5cdabc8fa7f1cea
Ankur Bansal <ankban@codeaurora.org>
2019-06-17 10:58:33 +0530

    Init : Add lito debug configuration
    Add lito debug configuration.
    
    Change-Id: I21704a7d4d3b60e779d33e757d3181d0418985d3

--- base.mk
+++ base.mk
@@ -1279,7 +1279,8 @@ PRODUCT_PACKAGES_DEBUG += \
     init.qti.debug-msmnile-slpi.sh \
     init.qti.debug-talos.sh \
     init.qti.debug-msmnile.sh \
-    init.qti.debug-kona.sh
+    init.qti.debug-kona.sh \
+    init.qti.debug-lito.sh
 
 PRODUCT_PACKAGES += liboemaids_system
 PRODUCT_PACKAGES += liboemaids_vendor
